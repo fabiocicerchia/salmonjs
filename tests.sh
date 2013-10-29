735986f8e11bcee1ee058b9e886551357496addd
Fabio Cicerchia <info@fabiocicerchia.it>
2013-10-29 19:22:10 +0000

    changed tests

--- tests.sh
+++ tests.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-[ $(./bin/crawler --uri "tests/test_01.html" | grep "Launching crawler" | wc -l) -eq "1" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_02.html" | grep "Launching crawler" | wc -l) -eq "2" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_03.html" | grep "Launching crawler" | wc -l) -eq "2" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_04.html" | grep "Launching crawler" | wc -l) -eq "4" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_05.html" | grep "Launching crawler" | wc -l) -eq "4" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_06.html" | grep "Launching crawler" | wc -l) -eq "6" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_07.html" | grep "Launching crawler" | wc -l) -eq "4" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_08.html" | grep "Launching crawler" | wc -l) -eq "4" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_09.html" | grep "Launching crawler" | wc -l) -eq "6" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_10.html" | grep "Launching crawler" | wc -l) -eq "6" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_11.html" | grep "Launching crawler" | wc -l) -eq "6" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_12.html" | grep "Launching crawler" | wc -l) -eq "2" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_13.html" | grep "Launching crawler" | wc -l) -eq "3" ] && echo "true" || echo "false" # TODO: is not getting the value from document.location
-[ $(./bin/crawler --uri "tests/test_14.html" | grep "Launching crawler" | wc -l) -eq "3" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_15.html" | grep "Launching crawler" | wc -l) -eq "8" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_16.html" | grep "Launching crawler" | wc -l) -eq "2" ] && echo "true" || echo "false"
-[ $(./bin/crawler --uri "tests/test_17.html" | grep "Launching crawler" | wc -l) -eq "3" ] && echo "true" || echo "false" # TODO: is not getting the value from onclick
+
+assert () {
+    echo -n "Test '$1': "
+    RESPONSE=$(./bin/crawler --uri "$1" | grep "Launching crawler" | wc -l)
+    [ "$RESPONSE" -eq "$2" ] && echo "true" || echo "false ($RESPONSE)"
+}
+
+assert "tests/test_01.html" "1"
+assert "tests/test_02.html" "2"
+assert "tests/test_03.html" "2"
+assert "tests/test_04.html" "4"
+assert "tests/test_05.html" "4"
+assert "tests/test_06.html" "6"
+assert "tests/test_07.html" "4"
+assert "tests/test_08.html" "4"
+assert "tests/test_09.html" "6"
+assert "tests/test_10.html" "6"
+assert "tests/test_11.html" "6"
+assert "tests/test_12.html" "2"
+assert "tests/test_13.html" "3" # TODO: is not getting the value from document.location
+assert "tests/test_14.html" "3"
+assert "tests/test_15.html" "8"
+assert "tests/test_16.html" "2"
+assert "tests/test_17.html" "3" # TODO: is not getting the value from onclick
